296fee122760b67b6be4fa5bf3408b6aebcb9085
ricardo <garnan92@gmail.com>
2024-06-29 14:56:16 -0600

    add page and relation sp

---
+++ UrlsScrapperPages/dbo/Stored Procedures/AddElementRelation.sql
@@ -0,0 +1,7 @@
+﻿CREATE PROCEDURE [dbo].AddElementRelation
+	@name varchar(250)
+AS
+	INSERT INTO [dbo].[ElementRelation]
+           ([name])
+     VALUES
+           (@name)
---
+++ UrlsScrapperPages/dbo/Stored Procedures/AddPage.sql
@@ -0,0 +1,36 @@
+﻿CREATE PROCEDURE [dbo].AddPage
+	@url varchar(max) = null,
+	@id_element_relation uniqueidentifier,
+	@name varchar(250),
+	@html varbinary(max) = null,
+	@json varbinary(max) = null
+AS
+	declare @id uniqueidentifier;
+
+	select @id = id from Pages where name = @name;
+
+	if @id is not null
+		UPDATE [dbo].[Pages]
+		SET  [id_element_relation] = @id_element_relation
+			,[url] = @url
+			,[html] = @html
+			,[json] = @json
+			,[modified_date] = getdate()
+		WHERE id = @id;
+	else
+		INSERT INTO [dbo].[Pages] 
+		(
+			 [id_element_relation]
+			,[name]
+			,[url]
+			,[html]
+			,[json]
+		) 
+		VALUES 
+		(
+			 @id_element_relation
+			,@name
+			,@url
+			,@html
+			,@json
+		);
